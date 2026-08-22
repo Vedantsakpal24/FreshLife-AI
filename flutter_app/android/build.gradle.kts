458c25e57a54bd43d62c068a2784ca70adb2d4c0
unknown <vedant.sakpal2006@gmail.com>
2026-08-22 10:32:18 +0530

    fix: inject namespace for legacy plugins and configure subprojects in build.gradle.kts

--- flutter_app/android/build.gradle.kts
+++ flutter_app/android/build.gradle.kts
@@ -26,6 +26,19 @@ subprojects {
             enabled = false
         }
     }
+    plugins.withId("com.android.library") {
+        val androidExt = project.extensions.findByName("android")
+        if (androidExt != null) {
+            try {
+                val getNamespace = androidExt.javaClass.methods.firstOrNull { it.name == "getNamespace" }
+                val setNamespace = androidExt.javaClass.methods.firstOrNull { it.name == "setNamespace" && it.parameterTypes.size == 1 && it.parameterTypes[0] == String::class.java }
+                val currentNamespace = getNamespace?.invoke(androidExt)
+                if (currentNamespace == null || currentNamespace == "") {
+                    setNamespace?.invoke(androidExt, "com.plugin.${project.name.replace("-", "_")}")
+                }
+            } catch (_: Exception) {}
+        }
+    }
     afterEvaluate {
         if (project.hasProperty("android")) {
             val androidExt = project.extensions.findByName("android")
